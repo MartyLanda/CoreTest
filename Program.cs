71f4223cafc642fe4198dfb2ca8e1b7f9132107e
MartyL <martyl@localhost.localdomain>
2016-08-11 10:03:51 -0700

    Commit Test

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ". Nice to meet you!");
+        }
+    }
+}
