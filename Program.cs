b91c19c236006a2a629f3de954992c9de290bb9c
MartyL <martyl@localhost.localdomain>
2016-08-12 16:09:15 -0700

    Added Multiply Code

--- Program.cs
+++ Program.cs
@@ -9,6 +9,14 @@ namespace ConsoleApplication
             Console.Write("Please Enter Your Name: ");
             string name = Console.ReadLine();
             Console.WriteLine("Hello " + name + ". Nice to meet you!");
+            string strNum = "";
+            decimal num = 0;
+            while(decimal.TryParse(strNum, out num) != true) {
+                Console.Write("Enter a Number I Can Multiply By 10: ");
+                strNum = Console.ReadLine();                
+            }
+            num = Convert.ToDecimal(strNum) * 10;
+            Console.WriteLine(strNum + " Times 10 = " + num);
         }
     }
 }
